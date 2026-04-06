4abd40c50c6cadaa5ce4df7157bf609db595d31a
christopher-buss <christopher.buss+github@protonmail.com>
2026-04-06 02:47:04 +0100

    build: add weld configuration files for core and jecs packages

---
+++ packages/core/weld.config.ts
@@ -0,0 +1,6 @@
+export default {
+	declaration: true,
+	entry: "out/init.luau",
+	output: "dist/core.luau",
+	project: "default.project.json",
+};
---
+++ packages/jecs/weld.config.ts
@@ -0,0 +1,7 @@
+export default {
+	declaration: true,
+	entry: "out/init.luau",
+	external: ["@rbxts/flux"],
+	output: "dist/jecs.luau",
+	project: "default.project.json",
+};
