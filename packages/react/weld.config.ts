f56f3026363e914b10aeaa1c2d8f18a193a30b47
christopher-buss <christopher.buss+github@protonmail.com>
2026-04-15 18:37:46 +0100

    build(react): create bundler file

--- packages/react/weld.config.ts
+++ packages/react/weld.config.ts
@@ -1,7 +1,7 @@
 export default {
 	declaration: true,
 	entry: "out/init.luau",
-	external: ["@rbxts/flux"],
+	external: ["@rbxts/flux", "@rbxts/react"],
 	output: "dist/init.luau",
 	project: "default.project.json",
 };
