3c64ea6a14e6c93b380f436ce628d6d6646c117d
christopher-buss <christopher.buss+github@protonmail.com>
2026-07-22 03:28:01 +0100

    style: remove unused override

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,39 +1,30 @@
 import { GLOB_SRC, GLOB_TESTS } from "@isentinel/eslint-config";
 import { isentinel } from "@isentinel/eslint-config/oxlint";
 
-export default isentinel(
-	{
-		name: "project/options",
-		ignores: ["fixtures", "_fixtures", "**/constants-generated.ts"],
-		jsPlugins: false,
-		options: {
-			typeAware: true,
-		},
-		react: true,
-		roblox: {
-			files: [`packages/*/*/${GLOB_SRC}`, `e2e/*/*/${GLOB_SRC}`],
-			filesTypeAware: [`packages/*/*/${GLOB_SRC}`, `e2e/*/*/${GLOB_SRC}`],
-		},
-		test: {
-			jest: {
-				extended: true,
-				files: [
-					...GLOB_TESTS.map((path) => `packages/*/*/src/${path}`),
-					...GLOB_TESTS.map((path) => `packages/*/*/test/${path}`),
-				],
-			},
-			vitest: {
-				extended: true,
-				files: [...GLOB_TESTS.map((path) => `scripts/${path}`)],
-			},
-		},
-		type: "package",
+export default isentinel({
+	name: "project/options",
+	ignores: ["fixtures", "_fixtures", "**/constants-generated.ts"],
+	jsPlugins: false,
+	options: {
+		typeAware: true,
 	},
-	{
-		name: "project/github-required-filenames",
-		files: [".github/FUNDING.{yml,yaml}", ".github/ISSUE_TEMPLATE/**"],
-		rules: {
-			"unicorn/filename-case": "off",
+	react: true,
+	roblox: {
+		files: [`packages/*/*/${GLOB_SRC}`, `e2e/*/*/${GLOB_SRC}`],
+		filesTypeAware: [`packages/*/*/${GLOB_SRC}`, `e2e/*/*/${GLOB_SRC}`],
+	},
+	test: {
+		jest: {
+			extended: true,
+			files: [
+				...GLOB_TESTS.map((path) => `packages/*/*/src/${path}`),
+				...GLOB_TESTS.map((path) => `packages/*/*/test/${path}`),
+			],
+		},
+		vitest: {
+			extended: true,
+			files: [...GLOB_TESTS.map((path) => `scripts/${path}`)],
 		},
 	},
-);
+	type: "package",
+});
